29039abcab9f663bc5d04a449f625cdde05ce4e6
unknown <vinaykumarbeelagi@gmail.com>
2024-02-21 18:42:15 +0530

    expressis installed and get method used

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+});
+
+app.get('/api/courses',(req,res)=> {
+    res.send(([1,2,3]))
+});
+app.get('/api/courses/:id',(req,res)=> {
+    res.send(req.params.id);
+});
+const port = process.env.PORT || 8080
+app.listen(port,()=> console.log(`listening on port${port}`))
