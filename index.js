bb67af56e2fcf11f438c193f179473e3e8c1cb8a
unknown <vinaykumarbeelagi@gmail.com>
2024-02-21 19:16:11 +0530

    routing example

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const app = express();
 
+const courses = [
+    {id:1, name:"course1"},
+    {id:2, name:"course2"},
+    {id:3, name:"course3"},
+];
+
 app.get('/',(req,res)=>{
     res.send('hello world')
 });
 
 app.get('/api/courses',(req,res)=> {
-    res.send(([1,2,3]))
+    res.send((courses))
 });
 app.get('/api/courses/:id',(req,res)=> {
-    res.send(req.params.id);
+    const course = courses.find(c=>c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send("course of give ID not found")
+    res.send(course)
 });
 const port = process.env.PORT || 8080
 app.listen(port,()=> console.log(`listening on port${port}`))
